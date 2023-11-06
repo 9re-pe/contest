166d9a519ae9678813ce5b8fe4288f70f3518577
9re-pe <1111yusuke@gmail.com>
2023-11-06 19:19:04 +0900

    practice

--- atcoder/abc_311/d.py
+++ atcoder/abc_311/d.py
@@ -1,82 +1,54 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**9)
 
 
-def dfs(i, j):
-    global mt
-    global visited
-    global status
+DIRECTIONS = [
+    (0, 1),   # 上
+    (0, -1),  # 下
+    (1, 0),   # 右
+    (-1, 0)   # 左
+]
 
-    print(i, j, status)
-    visited[i][j] = 1
 
-    if status == 'stop':
-        if i-1 >= 0 and mt[i-1][j] == 0 and visited[i-1][j] == 0:
-            status = 'up'
-            dfs(i-1, j)
-        if i+1 <= n-1 and mt[i+1][j] == 0 and visited[i+1][j] == 0:
-            status = 'down'
-            dfs(i+1, j)
-        if j-1 >= 0 and mt[i][j-1] == 0 and visited[i][j-1] == 0:
-            status = 'left'
-            dfs(i, j-1)
-        if j+1 <= m-1 and mt[i][j+1] == 0 and visited[i][j+1] == 0:
-            status = 'right'
-            dfs(i, j+1)
+def dfs(u):
+    # 元の位置に止まったら終了
+    if stopped[u[0]][u[1]] == 1:
         return
-    if status == 'up':
-        if i-1 >= 0 and mt[i-1][j] == 0:
-            dfs(i-1, j)
-        else:
-            status = 'stop'
-            dfs(i, j)
-    if status == 'down':
-        if i+1 <= n-1 and mt[i+1][j] == 0:
-            dfs(i+1, j)
-        else:
-            status = 'stop'
-            dfs(i, j)
-    if status == 'left':
-        if j-1 >= 0 and mt[i][j-1] == 0:
-            dfs(i, j-1)
-        else:
-            status = 'stop'
-            dfs(i, j)
-    if status == 'right':
-        if j+1 <= m-1 and mt[i][j+1] == 0:
-            dfs(i, j+1)
-        else:
-            status = 'stop'
-            dfs(i, j)
+
+    stopped[u[0]][u[1]] = 1
+
+    for d in DIRECTIONS:
+        v = move(u, d)
+        dfs(v)
+
+
+def move(u, d):
+    """
+    移動したマスのvisitedを1にし、終了地点を返す
+    :param u: 移動開始地点
+    :param d: 移動方向
+    :return: 終了地点
+    """
+    v = u
+    while True:
+        visited[v[0]][v[1]] = 1
+        nxt = tuple(x + y for x, y in zip(v, d))
+
+        # 移動方向の先にあるマスが岩なら終了
+        if grid[nxt[0]][nxt[1]] == '#':
+            break
+
+        v = nxt
+
+    return v
 
 
 n, m = map(int, input().split())
-mt = [[0 for j in range(m)] for i in range(n)]
-for i in range(n):
-    s = list(input())
-    for j in range(m):
-        if s[j] == '#':
-            mt[i][j] = 1
-
-visited = [[0 for j in range(m)] for i in range(n)]
-status = 'stop'
-dfs(1, 1)
-
-cnt = 0
-for i in range(n):
-    for j in range(m):
-        if mt[i][j] == 0 and visited[i][j] == 1:
-            cnt += 1
-print(cnt)
-
-# デバッグ用
-for i in range(n):
-    row = []
-    for j in range(m):
-        if mt[i][j] == 1:
-            row.append('#')
-        elif mt[i][j] == 0 and visited[i][j] == 1:
-            row.append('x')
-        else:
-            row.append('.')
-    print(''.join(map(str, row)))
+grid = [list(input()) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+stopped = [[0] * m for _ in range(n)]
+dfs((1, 1))
+ans = 0
+for row in visited:
+    ans += sum(row)
+print(ans)
